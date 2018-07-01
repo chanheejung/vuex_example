067b1613e3b8be12b9548ee7a90fc700b8e7c48a
kiba15@naver.com <kiba15ktoon>
2018-07-01 22:41:56 +0900

    기본 설정

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <div id="app">
-    <h1>{{ msg }}</h1>
-  </div>
+  <div>{{ count }}</div>
 </template>
 
 <script>
 export default {
-  el: '#app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      count: 0
+    }
+  },
+  // action
+  methods: {
+    increment () {
+      this.count++
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
