1c7157edaad00d05e74fce081ed36c5a1b96dd77
kiba15@naver.com <kiba15ktoon>
2018-07-01 22:50:38 +0900

    Vuex를 이용하지 않고 Parent --> Child 컴포넌트 데이터 공유
    * 추후 과제
     : Child 컴포넌트의 +/- 버튼 동작하도록 변경

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
-  <div>{{ count }}</div>
+  <div id="app">
+  Parent counter : {{ counter }} <br>
+  <button @click="addCounter">+</button>
+  <button @click="subCounter">-</button>
+
+  <!-- Child 컴포넌트를 등록하고 counter 데이터 속성을 props 로 전달한다. -->
+  <child v-bind:passedCounter="counter"></child>
+</div>
 </template>
 
 <script>
+import Child from './Child.vue'
+
 export default {
   data () {
     return {
-      count: 0
+      counter: 0
     }
   },
   // action
   methods: {
-    increment () {
-      this.count++
+    // 이벤트 추가
+    addCounter() {
+      this.counter++;
+    },
+    subCounter() {
+      this.counter--;
     }
+  },
+  components: {
+    // Child 컴포넌트를 하위 컴포넌트로 등록
+    'child': Child
   }
 }
 </script>
---
+++ src/Child.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <hr>
+    Child counter : {{ passedCounter }} <br>
+    <button>+</button>
+    <button>-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // Parent 에서 넘겨준 counter 속성을 passedCounter 로 받음
+  props: ['passedCounter']
+}
+</script>
+
+<style>
+
+</style>
