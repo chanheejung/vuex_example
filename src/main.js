25f4d0249f00d6a8c478839883cadc2266560d1b
kiba15@naver.com <kiba15ktoon>
2018-07-01 23:00:26 +0900

    Vuex 설치 및 설정
    $ npm install vuex --save

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// store.js 를 불러와
+import { store } from './store'
 
 new Vue({
   el: '#app',
+  // Vue 인스턴스에 등록한다.
+  store,
   render: h => h(App)
 })
